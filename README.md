1c68e691ddb1bdfdad4b59345ef166c3eb09cc72
Maximilian Geitner <maximilian.geitner@outlook.de>
2023-12-01 17:41:04 +0100

    Progress Day One

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023
+
+This repository contains my own solution approaches for the 2023 problems on the programming-puzzle website [Advent of Code](https://adventofcode.com/).
+
+My programming solutions are written in Python. 
---
+++ day_01_trebuchet/exercise_01.py
@@ -0,0 +1,44 @@
+
+__author__ = "Maximilian Geitner"
+__date__ = "01.12.2023"
+
+if __name__ == '__main__':
+    filename = "input.txt"
+    # Day 01 Part One
+    with open(filename) as file:
+        total = 0
+        for line in file:
+            line = line.replace("\n", "")
+            digits = ""
+            # parse all characters in line, extract all digits
+            for c in line:
+                if '0' <= c <= '9':
+                    digits += c
+            # use first and last digit for computing the solution
+            if digits != "":
+                digits_i = int(digits[0] + digits[-1])
+                total += digits_i
+        print('Solution Part One: ', total)
+
+    # Day 01 Part Two
+    total = 0
+    with open(filename) as file:
+        for line in file:
+            line = line.replace("\n", "")
+            digits = ""
+            digits_str = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+            for index, c in enumerate(line):
+                if '0' <= c <= '9':
+                    digits += c
+                else:
+                    for index_val, text in enumerate(digits_str):
+                        len_val = len(text)
+                        if index + len_val <= len(line):
+                            # compare substring with digits as text
+                            if line[index:].startswith(text):
+                                digits += str(index_val)
+            # use first and last digit for computing the solution
+            if digits != "":
+                digits_i = int(digits[0] + digits[-1])
+                total += digits_i
+    print('Solution Part Two: ', total)
